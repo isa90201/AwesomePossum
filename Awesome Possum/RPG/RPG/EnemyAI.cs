e60f7fce9e2935305f6c5a7fac8a18a6465aeb6f
Jesus Arevalo <forcs113@gmail.com>
2014-03-05 20:21:02 -0800

    * Made the AI less erratic.

--- Awesome Possum/RPG/RPG/EnemyAI.cs
+++ Awesome Possum/RPG/RPG/EnemyAI.cs
@@ -7,15 +7,36 @@ namespace RPG
 {
     public class EnemyAI
     {
+        enum States
+        {
+            IDLE,
+            WALKING
+        }
+
         private const int UPPER_BOUND = 100;            //random number generator
 
         private int DifficultyNumber;
         private Random RandomNumber;
 
+        private int MinWalkTime;
+        private int MaxWalkTime;
+
+        private int MinWaitTime;
+        private int MaxWaitTime;
+
+        private int WaitTime;
+        private States State;
+
         public EnemyAI(int ai_difficultyNumber)  //Assigned difficulty
         {
             DifficultyNumber = ai_difficultyNumber;
             RandomNumber = new Random();
+
+            MaxWalkTime = ai_difficultyNumber * 40 + 1000;
+            MinWalkTime = ai_difficultyNumber * 27 + 250;
+
+            MaxWaitTime = ai_difficultyNumber * -25 + 3000;
+            MinWaitTime = ai_difficultyNumber * -10 + 1000;
         }
 
         public bool IsAttacking()
@@ -25,7 +46,21 @@ namespace RPG
 
         public bool IsMoving()
         {
-            return RandomNumber.Next(UPPER_BOUND) < DifficultyNumber;
+            if (WaitTime < Environment.TickCount)
+            {
+                if (State == States.IDLE)
+                {
+                    State = States.WALKING;
+                    WaitTime = Environment.TickCount + RandomNumber.Next(MinWalkTime, MaxWalkTime);
+                }
+                else
+                {
+                    State = States.IDLE;
+                    WaitTime = Environment.TickCount + RandomNumber.Next(MinWaitTime, MaxWaitTime);
+                }
+            }
+
+            return State == States.WALKING;
         }
     }
 }
